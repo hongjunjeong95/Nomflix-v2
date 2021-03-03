3a8d2ab1a916ff60398cd58f56ade5666838323c
zpskek <wjdghdwns0@gmail.com>
2021-03-03 18:46:58 +0900

    Create GlobalStyles

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Router from "Components/Router";
+import GlobalStyles from "Components/GlobalStyles";
 
 class App extends React.Component {
   render() {
-    return <Router />;
+    return (
+      <>
+        <GlobalStyles />
+        <Router />
+      </>
+    );
   }
 }
 
