d559a4ac0a3536f4a9a184a94b65084c740e8449
zpskek <wjdghdwns0@gmail.com>
2021-04-28 08:09:59 +0900

    Create header

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -8,16 +8,20 @@ import Movie from "Routes/Movie";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
 import Detail from "Routes/Detail";
+import Header from "./Header";
 
 export default () => (
   <Router>
-    <Switch>
-      <Route exact path="/" component={Movie} />
-      <Route exact path="/tv" component={TV} />
-      <Route exact path="/search" component={Search} />
-      <Route exact path="/movie/:id" component={Detail} />
-      <Route exact path="/tv/:id" component={Detail} />
-      <Redirect from="*" to="/"></Redirect>
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Movie} />
+        <Route exact path="/tv" component={TV} />
+        <Route exact path="/search" component={Search} />
+        <Route exact path="/movie/:id" component={Detail} />
+        <Route exact path="/tv/:id" component={Detail} />
+        <Redirect from="*" to="/"></Redirect>
+      </Switch>
+    </>
   </Router>
 );
