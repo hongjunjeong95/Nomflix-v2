eaeb6001e5c9fd132fdb3048586fcf394fd50430
zpskek <wjdghdwns0@gmail.com>
2021-04-28 08:21:47 +0900

    Create MovieContainer

--- src/Routes/Movie/MoviePresenter.js
+++ src/Routes/Movie/MoviePresenter.js
@@ -1,2 +1,11 @@
-const MoviePresenter = () => <h1>movie</h1>;
+import Helmet from "react-helmet";
+
+const MoviePresenter = ({ nowPlaying, upcoming, popular, error, loading }) => (
+  <>
+    <Helmet>
+      <title>Movies | Nomflix</title>
+    </Helmet>
+    {loading ? <h1>loading</h1> : <h2>heloo</h2>}
+  </>
+);
 export default MoviePresenter;
