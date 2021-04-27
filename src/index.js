1c687b13adf73e577a2c567f32b38074d4971166
zpskek <wjdghdwns0@gmail.com>
2021-04-28 07:38:05 +0900

    Create api.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import dotenv from "dotenv";
+
+import App from "./Components/App";
+
+dotenv.config();
 
 ReactDOM.render(
   <React.StrictMode>
