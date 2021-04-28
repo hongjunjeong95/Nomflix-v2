f2da2df218f01be0c3210efe955526b4fb0175bd
zpskek <wjdghdwns0@gmail.com>
2021-04-28 09:32:43 +0900

    Create tv container and presenter

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,3 +1,71 @@
-const TVPresenter = () => <h1>TV</h1>;
+import Helmet from "react-helmet";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+import Loader from "Components/Loader";
+import Poster from "Components/Poster";
+import Section from "Components/Section";
+
+const Container = styled.div`
+  padding: 10px;
+`;
+
+const TVPresenter = ({ airingToday, topRated, popular, error, loading }) => (
+  <>
+    <Helmet>
+      <title>TV | Nomflix</title>
+    </Helmet>
+    {loading ? (
+      <Loader />
+    ) : (
+      <Container>
+        <Section title="Now Playing">
+          {airingToday.map((tv) => (
+            <Poster
+              key={tv.id}
+              id={tv.id}
+              imageUrl={tv.poster_path}
+              title={tv.original_name}
+              rating={tv.vote_average}
+              year={tv.first_air_date.substring(0, 4)}
+            />
+          ))}
+        </Section>
+        <Section title="Upcoming">
+          {topRated.map((tv) => (
+            <Poster
+              key={tv.id}
+              id={tv.id}
+              imageUrl={tv.poster_path}
+              title={tv.original_name}
+              rating={tv.vote_average}
+              year={tv.first_air_date.substring(0, 4)}
+            />
+          ))}
+        </Section>
+        <Section title="Popular">
+          {popular.map((tv) => (
+            <Poster
+              key={tv.id}
+              id={tv.id}
+              imageUrl={tv.poster_path}
+              title={tv.original_name}
+              rating={tv.vote_average}
+              year={tv.first_air_date.substring(0, 4)}
+            />
+          ))}
+        </Section>
+      </Container>
+    )}
+  </>
+);
+
+TVPresenter.propTypes = {
+  topRated: PropTypes.array,
+  popular: PropTypes.array,
+  airingToday: PropTypes.array,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string,
+};
 
 export default TVPresenter;
